bb2b25dd52da4753c25cc277f42ac33cca760a76
k4m1 <k4m1@protonmail.com>
2024-10-27 12:20:06 +0100

    Start working on ivt

---
+++ src/include/interrupts/ivt.h
@@ -0,0 +1,51 @@
+/*
+ BSD 3-Clause License
+ 
+ Copyright (c) 2024, k4m1
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+ 
+ 3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#ifndef __IVT_H__
+#define __IVT_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* Helper to register interrupt handler for a driver or other bit of
+ * software that wants to listen to interrupts.
+ *
+ * @param uint16_t segment -- what CS value should we have upon jump to handler
+ * @param uint16_t offset  -- where in that segment our handler is at? 
+ * @param uint16_t entry   -- which interrupt are we handling
+ */
+static inline void register_int_handler(uint16_t segment, uint16_t offset, uint16_t entry) {
+    uint16_t *ptr = (uint16_t *)(entry * 4);
+    *ptr = segment;
+    *ptr++ = offset;
+}
+
+#endif // __IVT_H__
---
+++ src/interrupts/ivt.c
@@ -0,0 +1,59 @@
+/*
+ BSD 3-Clause License
+ 
+ Copyright (c) 2024, k4m1
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+ 
+ 3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <interrupts/ivt.h>
+
+/* Dummy/default interrupt handler.
+ *
+ */
+static void __attribute__((section(".rom_int_handler"))) default_int_handler(void) {
+    
+}
+
+/* Initialise interrupt vector table to only contain
+ * dummy handlers that send EOI and return.
+ *
+ */
+void init_ivt(void) {
+    uint32_t default_entry = 0xFFFF0000 | ((uint16_t)((uint32_t)default_int_handler) & 0x0000FFFF);
+
+    for (uint32_t *ivt_entry = 0; ivt_entry < 0x200; ivt_entry++) {
+        *ivt_entry = default_entry;
+    }
+}
+
+
+
+
