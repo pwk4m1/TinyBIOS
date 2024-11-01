d258eb8a088ac606dfe0ee13ace4ac2f0535a354
k4m1 <k4m1@protonmail.com>
2024-11-01 22:31:48 +0100

    Rework interrupt handling

---
+++ src/interrupts/interrupts.c
@@ -0,0 +1,62 @@
+/*
+ BSD 3-Clause License
+ 
+ Copyright (c) 2024, k4m1
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ 1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+ 
+ 3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <sys/io.h>
+#include <sys/cpu.h>
+#include <stdint.h>
+
+extern void switch_to_protected(void);
+extern void switch_to_unreal(void);
+
+/* Setup suitable environment for our interrupt handler. We expect that 
+ * interrupts have been disabled at this point already, 
+ * and necessary registers have been stored.
+ * XXX: Necessary registers include gdtr and all but code segment.
+ *
+ * start by checking if we're in protected or unreal mode already, if we are
+ * there's nothing for us to do.
+ *
+ * Then, jump around until we're in unreal mode.
+ */
+void __attribute__((section(".rom_int_handler"))) int_handler_scrt(void) {
+    uint32_t cr0 = read_cr0();
+    uint16_t ds = read_ds();
+
+    if ((cr0 & CR0_PE) || (ds == 8)) {
+        return;
+    }
+
+    switch_to_protected();
+    switch_to_unreal();
+
+}
+
